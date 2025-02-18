c5d17e51709ae980da70ef75dbb2ad94c415aaa9
altomizawa <alyssontomizawa@hotmail.com>
2025-02-18 12:50:08 -0300

    style: add css to header

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Tektur } from "next/font/google";
+import Link from 'next/link';
 import "./globals.css";
 import {
   ClerkProvider,
@@ -7,6 +8,9 @@ import {
   SignedOut,
   UserButton
 } from '@clerk/nextjs'
+
+import { auth, currentUser } from '@clerk/nextjs/server'
+
 
 const tektur = Tektur({
   variable: "--font-tektur",
@@ -19,18 +23,53 @@ export const metadata = {
   description: "Simple authentication using Clerk",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const { userId } = await auth();
+  const user = await currentUser();
+  const userEmail = user?.emailAddresses[0].emailAddress
+
   return (
     <ClerkProvider>
       <html lang="en">
         <body
           className={`${tektur.variable} antialiased`}
         >
-          <SignedOut>
-            <SignInButton />
+          <SignedOut><div className='mt-12 px-12 flex items-center '>
+              <ul className='flex w-full justify-between items-center'>
+                <SignInButton>
+                  <button className='text-black w-max border-2 border-black px-4 py-2 rounded-md duration-300 hover:bg-black hover:text-white' style={{ fontFamily: 'var(--font-tektur)' }}>LOG IN</button>
+                </SignInButton>
+                <li className='font-bold uppercase duration-300 hover:text-gray-400'>
+                  <Link href="/">Home</Link>
+                </li>
+                <li className='font-bold uppercase duration-300 hover:text-gray-400'>
+                  <Link href="/dashboard">Dashboard</Link>
+                </li>
+                <li className='font-bold uppercase duration-300 hover:text-gray-400'>
+                  <Link href="/api/auth/signout">My Crypto</Link>
+                </li>
+              </ul>
+            </div>
           </SignedOut>
           <SignedIn>
-            <UserButton />
+            <div className='mt-12 pl-8 flex '>
+              <div className='flex items-center w-min'>
+                <UserButton />
+                <p>Hello, {userEmail}</p>
+
+              </div>
+              <ul className='flex w-full justify-around'>
+                <li className='font-bold uppercase duration-300 hover:text-gray-400'>
+                  <Link href="/">Home</Link>
+                </li>
+                <li className='font-bold uppercase duration-300 hover:text-gray-400'>
+                  <Link href="/dashboard">Dashboard</Link>
+                </li>
+                <li className='font-bold uppercase duration-300 hover:text-gray-400'>
+                  <Link href="/api/auth/signout">My Crypto</Link>
+                </li>
+              </ul>
+            </div>
           </SignedIn>
           {children}
         </body>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-items-center p-8 pb-20 sm:p-20">
-      <Image src="/cryptexion_bkg.png" width={1920} height={1080} alt="Cryptexion Logo" className='w-screen h-screen fixed top-0 left-0 -z-10 opacity-50' />
-        <h3 className='text-[2rem] uppercase' style={{ fontFamily: 'var(--font-tektur)' }}>Welcome to</h3>
-        <h1 className='text-[5rem] uppercase' style={{ fontFamily: 'var(--font-tektur)' }}>Cryptexion</h1>
+    <div className='bg-black z-0 relative'>
+      <div className='mt-12 overflow-hidden relative h-[70vh] flex flex-col justify-center pl-[20%]'>
+        <Image src="/cryptexion_bkg2.png" priority='true' width={1920} height={1080} alt="cryptexionion background image" className='absolute top-0 left-0 w-screen h-screen opacity-90 -z-10' />
+        <h3 className='text-[2rem] uppercase text-white w-full' style={{ fontFamily: 'var(--font-tektur)' }}>Welcome to</h3>
+        <h1 className='text-[8rem] uppercase text-white mt-[-2rem] translate-x-[-0.5rem]' style={{ fontFamily: 'var(--font-tektur)' }}>cryptexionion</h1>
+        <button className='underline text-xl text-white underline-offset-8 w-max px-4 py-2 mt-[-1.5rem] tracking-widest' style={{ fontFamily: 'var(--font-tektur)' }}>LEARN MORE</button>
+      </div>
+      <div className='bg-white h-[50vh] flex flex-col justify-center items-center'>
+        <h3 className='text-black text-3xl font-extrabold'>THE BEST PLACE TO BUY AND SELL CRYPTO</h3>
+        <p className='mt-12 w-1/2 leading-9' style={{ fontFamily: 'var(--font-tektur)' }} >Cryptexion is your trusted platform for buying and selling cryptocurrencies with ease and security. Whether you're a seasoned trader or just getting started, we provide a seamless experience with real-time market data, low fees, and top-tier security. Our user-friendly interface and robust trading tools make it simple to navigate the crypto world and execute trades with confidence. Join cryptexion today and take control of your digital assets like never before!</p>
+      </div>
     </div>
   );
 }
